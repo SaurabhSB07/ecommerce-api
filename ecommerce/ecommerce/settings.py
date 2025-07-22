641bdf802258fcd1d40c7a71283b1b49dfb2f89d
SaurabhSB07 <barkesaurabh07@gmail.com>
2025-07-22 20:04:59 +0530

    Globle Pagination is Added

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -148,3 +148,8 @@ SIMPLE_JWT = {
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
+}
